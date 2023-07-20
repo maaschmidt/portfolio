2cf896e8dc0a044294783535bbf2f94ce26bd4a0
Marcel Schmidt <marcel.schmidt@universo.univates.br>
2023-07-20 12:01:26 -0300

    fix: social in contact page

--- src/components/about/socials.jsx
+++ src/components/about/socials.jsx
@@ -14,6 +14,22 @@ import "./styles/socials.css";
 const Socials = () => {
 	return (
 		<div className="socials">
+			<div className="email">
+				<div className="email-wrapper">
+					<a
+						href={`mailto:${INFO.main.email}`}
+						target="_blank"
+						rel="noreferrer"
+					>
+						<div className="social-icon">
+							<FontAwesomeIcon icon={faEnvelope} />
+						</div>
+
+						<div className="social-text">{INFO.main.email}</div>
+					</a>
+				</div>
+			</div>
+
 			<div className="social">
 				<a href={INFO.socials.linkedin} target="_blank" rel="noreferrer">
 					<div className="social-icon">
@@ -39,22 +55,6 @@ const Socials = () => {
 					</div>
 					<div className="social-text">Follow on Instagram</div>
 				</a>
-			</div>
-
-			<div className="email">
-				<div className="email-wrapper">
-					<a
-						href={`mailto:${INFO.main.email}`}
-						target="_blank"
-						rel="noreferrer"
-					>
-						<div className="social-icon">
-							<FontAwesomeIcon icon={faEnvelope} />
-						</div>
-
-						<div className="social-text">{INFO.main.email}</div>
-					</a>
-				</div>
 			</div>
 		</div>
 	);
